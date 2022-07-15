eed0b6d4e2257f5cefb220376560d196de31d308
Sachin Nihalani <36417253+sachinn03@users.noreply.github.com>
2022-07-15 21:36:08 +0530

    Don't go Outside in Odd day

---
+++ EvenOdd.py
@@ -0,0 +1,14 @@
+size = int(input("Enter the size : "))
+lst = []
+even_count=0
+odd_count=0
+for i in range(1,size+1):
+    lst.append(i)
+print(lst)
+for unit in lst:
+    if unit % 2 == 0:
+        even_count = even_count + 1
+    else:
+        odd_count = odd_count + 1
+print("Number of even number :", even_count)
+print("Number of odd number :", odd_count)
