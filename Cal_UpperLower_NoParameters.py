b9f11fd5f640a6805eb918beec66e294bf2f8b27
Sachin Nihalani <36417253+sachinn03@users.noreply.github.com>
2022-07-30 11:08:49 +0530

    Calculate the Upper and The lower Case

---
+++ Cal_UpperLower_NoParameters.py
@@ -0,0 +1,20 @@
+def calculate():
+    user_input = input("Enter a string: ").strip()
+    cap_count = 0
+    low_count = 0
+    s = ''
+    for k in user_input:
+        if k == " ":
+            continue
+        else:
+            s = s + k
+    for i in range(len(s)):
+        if s[i] == s[i].upper():
+            cap_count = cap_count + 1
+        else:
+            low_count = low_count + 1
+    print("No. of upper case characters: ",cap_count)
+    print("No. of upper case characters: ",low_count)
+    return cap_count, low_count
+
+calculate()
